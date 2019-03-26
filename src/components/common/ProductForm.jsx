e05b356ec35a51cb5bfb1ba0e8c84ef18e7b12c5
Nick Rameau <r@meau.me>
2019-03-26 15:08:02 -0400

    [Patch #2] Display units in the preferred unit system

--- src/components/common/ProductForm.jsx
+++ src/components/common/ProductForm.jsx
@@ -13,16 +13,17 @@ import SalesChannelDropdown from './SalesChannelDropdown';
 import ImageUpload from './ImageUpload';
 import Button from './Button';
 import ProductCategoryDropdown from './ProductCategoryDropdown';
-import units from '../constants/units';
 import CheckboxField from './CheckboxField';
 import { required, exactLength3 } from './validate';
+import convert from 'convert-units';
+import * as utils from '../../utils/index';
 
 const propTypes = {
   handleSubmit: PropTypes.func.isRequired
 };
 
 const defaultProps = {
-  handleSubmit: () => {}
+  handleSubmit: () => { }
 };
 
 const buttonToolbar = {
@@ -54,167 +55,177 @@ const renderPricing = ({ fields, meta: { error, submitFailed } }) => {
   return renderRow;
 };
 
-const ProductForm = ({ handleSubmit, ...props }) => (
-  <Form horizontal onSubmit={handleSubmit}>
-    <Grid>
-      <Row>
-        <Col md={8}>
-          <Field
-            name="id"
-            label="Product ID"
-            component={TextField}
-            horizontal
-            disabled
-          />
-          <Field
-            required
-            name="active"
-            label="Active"
-            component={CheckboxField}
-          />
-          <Field
-            name="name"
-            label="Product Name"
-            component={TextField}
-            horizontal
-            required
-            validate={[required]}
-          />
-          <Field
-            name="sku"
-            label="SKU"
-            component={TextField}
-            horizontal
-            required
-            validate={[required]}
-          />
-          <Field
-            name="description"
-            label="Product Description"
-            component={TextField}
-            horizontal
-            required
-            validate={[required]}
-          />
-          <Field
-            name="category"
-            label="Product Category"
-            component={ProductCategoryDropdown}
-            horizontal
-            required
-            validate={[required]}
-          />
-          <Field
-            name="priceAmount"
-            label="Default Price"
-            component={TextField}
-            horizontal
-            required
-            validate={[required]}
-            type="number"
-          />
-          <Field
-            name="priceCurrency"
-            label="Default Currency"
-            component={TextField}
-            horizontal
-            required
-            validate={[required, exactLength3]}
-          />
-          <Field
-            name="costOfGoods"
-            label="Cost of Goods"
-            component={TextField}
-            horizontal
-            required
-            validate={[required]}
-            type="number"
-          />
-          <Field
-            name="minQuantity"
-            label="Minimum Quantity"
-            component={TextField}
-            horizontal
-            type="number"
-          />
-          <Field
-            name="maxQuantity"
-            label="Maximum Quantity"
-            component={TextField}
-            horizontal
-            type="number"
-          />
-          <Field
-            name="unitsPerProduct"
-            label="Units per Product"
-            component={TextField}
-            horizontal
-            required
-            validators={[required]}
-            type="number"
-          />
-          <Field
-            name="unitMeasurement"
-            label="Units of Measurement"
-            component={SelectField}
-            options={units}
-            horizontal
-            required
-            validators={[required]}
-          />
-        </Col>
-        <Col md={4}>
-          <Field name="image" label="image" component={ImageUpload} />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={10}>
-          <Row style={{ paddingBottom: '10px' }}>
-            <Button
-              onClick={() => {
-                props.addMrp('productForm', 'productMrp', {
-                  productId: props.initialValues.id,
-                  active: true
-                });
-              }}
-              buttonText="Add MRP"
-              buttonSize="small"
-              buttonStyle="primary"
-              className="pull-right"
+const ProductForm = ({ handleSubmit, ...props }) => {
+  const preferredUnitSystem = utils.getSettingsItem(props.settings, 'default_unit_system');
+  const supportedMeasures = ['length', 'mass', 'volume'];
+  const unitMeasures = convert()
+                          .list()
+                          .filter(unit => unit.system === preferredUnitSystem.value)
+                          .filter(unit => supportedMeasures.includes(unit.measure))
+                          .map(unit => ({ label: unit.singular.toLowerCase(), value: unit.singular.toLowerCase() }));
+
+  return (
+    <Form horizontal onSubmit={handleSubmit}>
+      <Grid>
+        <Row>
+          <Col md={8}>
+            <Field
+              name="id"
+              label="Product ID"
+              component={TextField}
+              horizontal
+              disabled
             />
-          </Row>
-          <Row>
-            <Table striped bordered hover responsive>
-              <thead>
-                <tr>
-                  <th>Kiosk</th>
-                  <th>Sales Channel</th>
-                  <th>Amount</th>
-                  <th>Active</th>
-                </tr>
-              </thead>
-              <tbody>
-                <FieldArray name="productMrp" component={renderPricing} />
-              </tbody>
-            </Table>
-          </Row>
-        </Col>
-      </Row>
-    </Grid>
-    <ButtonToolbar className="text-center" style={buttonToolbar}>
-      <Button
-        buttonText="Cancel"
-        onClick={() => props.history.push('/products')}
-      />
-      <Button
-        onClick={handleSubmit}
-        type="submit"
-        buttonText="Save"
-        buttonStyle="primary"
-      />
-    </ButtonToolbar>
-  </Form>
-);
+            <Field
+              required
+              name="active"
+              label="Active"
+              component={CheckboxField}
+            />
+            <Field
+              name="name"
+              label="Product Name"
+              component={TextField}
+              horizontal
+              required
+              validate={[required]}
+            />
+            <Field
+              name="sku"
+              label="SKU"
+              component={TextField}
+              horizontal
+              required
+              validate={[required]}
+            />
+            <Field
+              name="description"
+              label="Product Description"
+              component={TextField}
+              horizontal
+              required
+              validate={[required]}
+            />
+            <Field
+              name="category"
+              label="Product Category"
+              component={ProductCategoryDropdown}
+              horizontal
+              required
+              validate={[required]}
+            />
+            <Field
+              name="priceAmount"
+              label="Default Price"
+              component={TextField}
+              horizontal
+              required
+              validate={[required]}
+              type="number"
+            />
+            <Field
+              name="priceCurrency"
+              label="Default Currency"
+              component={TextField}
+              horizontal
+              required
+              validate={[required, exactLength3]}
+            />
+            <Field
+              name="costOfGoods"
+              label="Cost of Goods"
+              component={TextField}
+              horizontal
+              required
+              validate={[required]}
+              type="number"
+            />
+            <Field
+              name="minQuantity"
+              label="Minimum Quantity"
+              component={TextField}
+              horizontal
+              type="number"
+            />
+            <Field
+              name="maxQuantity"
+              label="Maximum Quantity"
+              component={TextField}
+              horizontal
+              type="number"
+            />
+            <Field
+              name="unitsPerProduct"
+              label="Units per Product"
+              component={TextField}
+              horizontal
+              required
+              validators={[required]}
+              type="number"
+            />
+            <Field
+              name="unitMeasurement"
+              label="Unit of Measurement"
+              component={SelectField}
+              options={unitMeasures}
+              horizontal
+              required
+              validators={[required]}
+            />
+          </Col>
+          <Col md={4}>
+            <Field name="image" label="image" component={ImageUpload} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={10}>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Button
+                onClick={() => {
+                  props.addMrp('productForm', 'productMrp', {
+                    productId: props.initialValues.id,
+                    active: true
+                  });
+                }}
+                buttonText="Add MRP"
+                buttonSize="small"
+                buttonStyle="primary"
+                className="pull-right"
+              />
+            </Row>
+            <Row>
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Kiosk</th>
+                    <th>Sales Channel</th>
+                    <th>Amount</th>
+                    <th>Active</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <FieldArray name="productMrp" component={renderPricing} />
+                </tbody>
+              </Table>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+      <ButtonToolbar className="text-center" style={buttonToolbar}>
+        <Button
+          buttonText="Cancel"
+          onClick={() => props.history.push('/products')}
+        />
+        <Button
+          onClick={handleSubmit}
+          type="submit"
+          buttonText="Save"
+          buttonStyle="primary"
+        />
+      </ButtonToolbar>
+    </Form>
+  )
+}
 
 ProductForm.propTypes = propTypes;
 ProductForm.defaultProps = defaultProps;
